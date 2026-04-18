d6074025669462549fb69a0f4f46183e54576c15
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-04-18 02:55:45 -0400

    Initial Commit

---
+++ sumire-keyboard/sumire_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  sumire_keyboardApp.swift
+//  sumire-keyboard
+//
+//  Created by Kazuma on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct sumire_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sumire-keyboardTests/sumire_keyboardTests.swift
@@ -0,0 +1,19 @@
+//
+//  sumire_keyboardTests.swift
+//  sumire-keyboardTests
+//
+//  Created by Kazuma on 2026-04-18.
+//
+
+import Testing
+@testable import sumire_keyboard
+
+struct sumire_keyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
